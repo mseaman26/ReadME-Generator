90ed61c193b3a6b89fbd7641340c67f36c1d1963
michael seaman <mseaman26@gmail.com>
2022-10-21 18:33:43 -0700

    added .gitignore and refined table of contents

--- assets/index.js
+++ assets/index.js
@@ -19,7 +19,7 @@ inquirer
       type: "list",
       name: "license",
       message: "What license would you like to use for this project?",
-      choices: ["Apache License 2.0", "GNU General Public License v3.0", "MIT License", "BSD 2-Clause \"Simplified\" License", `BSD 3-Clause "New" or "Revised" License`, `Boost Software License 1.0`, `Creative Commons Zero v1.0 Universal`, `Eclipse Public License 1.0`, `GNU Affero General Public License v3.0`, `GNU General Public License v2.0`, `GNU Lesser General Public License v3.0`, `Mozilla Public License 2.0`, ` ` ]
+      choices: ["Apache License 2.0", "GNU General Public License v3.0", "MIT License", "BSD 2-Clause \"Simplified\" License", `BSD 3-Clause "New" or "Revised" License`, `Boost Software License 1.0`, `Creative Commons Zero v1.0 Universal`, `Eclipse Public License 1.0`, `GNU Affero General Public License v3.0`, `GNU General Public License v2.0`, `GNU Lesser General Public License v3.0`, `Mozilla Public License 2.0`, `The Unlicense` ]
 
     },
     {
@@ -73,15 +73,15 @@ inquirer
 
   ## Table of Contents
 
-  <a href="#Installation">Installation</a><br />
-  <a href="#Usage">Usage</a><br />
-  <a href="#How-to-Contribute-to-This-Repository">Contributing</a><br />
-  <a href="#To-run-tests-run-the-following-command">Tests</a><br />
-  <a href="#Questions">Questions</a><br />
+  * [Installation](#installation)<br />
+  * [Usage](#usage)<br />
+  * [Contributing to This Repository](#how-to-contribute-to-this-repository)<br />
+  * [Tests](#to-run-tests-run-the-following-command)<br />
+  * [Questions](#questions)<br />
 
   ## Installation
 
-    To install the neccessary dependancies, run the following command:
+    To install the necessary dependancies, run the following command:
 
       ${data.installation}
 
